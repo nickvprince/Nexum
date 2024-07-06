067251797d2cdc9f3b9abd99cfc52e8dc317ceb3
Islam Ahmed <islamdoesart@gmail.com>
2024-07-05 21:54:21 -0400

    NEX-261 NEX-264 NEX-165 Web App Http connector added

--- MSP App/SharedComponents/Entities/DeviceJobSchedule.cs
+++ MSP App/SharedComponents/Entities/DeviceJobSchedule.cs
@@ -1,9 +1,9 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SharedComponents.Entities
---
+++ MSP App/SharedComponents/Services/IJobService.cs
@@ -0,0 +1,22 @@
+﻿using SharedComponents.Entities;
+using SharedComponents.WebRequestEntities.DeviceRequests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedComponents.Services
+{
+    public interface IJobService
+    {
+        public Task<DeviceJob?> CreateAsync(DeviceCreateRequest request);
+        public Task<DeviceJob?> UpdateAsync(DeviceUpdateRequest request);
+        public Task<bool> DeleteAsync(int id);
+        public Task<DeviceJob?> GetAsync(int id);
+        public Task<ICollection<DeviceJob>?> GetAllAsync();
+        public Task<ICollection<DeviceJob>?> GetAllByDeviceIdAsync(int deviceId);
+        public Task<ICollection<DeviceJob>?> GetAllByTenantIdAsync(int tenantId);
+        public Task<ICollection<DeviceJob>?> GetAllByNASServerIdAsync(int nasServerId);
+    }
+}
