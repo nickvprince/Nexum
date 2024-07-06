7ff4f298c6f0e54f26ac4e8f1dbe5091c891886b
Islam Ahmed <islamdoesart@gmail.com>
2024-07-05 20:14:58 -0400

    NEX-314 Schedule Adapter updated

--- MSP App/SharedComponents/Utilities/ScheduleUtilities.cs
+++ MSP App/SharedComponents/Utilities/ScheduleUtilities.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharedComponents.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,32 +9,34 @@ namespace SharedComponents.Utilities
 {
     public class ScheduleUtilities
     {
-        public static string ConvertToTenantServerFormat(DayOfWeek[] days)
+        public static string ConvertToTenantServerFormat(DeviceJobSchedule schedule)
         {
-            if (days.Length != 7)
-            {
-                throw new ArgumentException("Schedule string must be 7 characters long.");
-            }
-            char[] schedule = new char[] { '0', '0', '0', '0', '0', '0', '0' };
-            foreach (var day in days)
-            {
-                schedule[(int)day] = '1';
-            }
-            return new string(schedule);
+            return $"{(schedule.Sunday ? '1' : '0')}" +
+                $"{(schedule.Monday ? '1' : '0')}" +
+                $"{(schedule.Tuesday ? '1' : '0')}" +
+                $"{(schedule.Wednesday ? '1' : '0')}" +
+                $"{(schedule.Thursday ? '1' : '0')}" +
+                $"{(schedule.Friday ? '1' : '0')}" +
+                $"{(schedule.Saturday ? '1' : '0')}";
         }
 
-        public static DayOfWeek[] ConvertFromTenantServerFormat(string schedule)
+        public static DeviceJobSchedule ConvertFromTenantServerFormat(string schedule)
         {
             if (schedule.Length != 7)
             {
                 throw new ArgumentException("Schedule string must be 7 characters long.");
             }
 
-            return schedule
-                .Select((c, index) => new { c, index })
-                .Where(x => x.c == '1')
-                .Select(x => (DayOfWeek)x.index)
-                .ToArray();
+            return new DeviceJobSchedule
+            {
+                Sunday = schedule[0] == '1',
+                Monday = schedule[1] == '1',
+                Tuesday = schedule[2] == '1',
+                Wednesday = schedule[3] == '1',
+                Thursday = schedule[4] == '1',
+                Friday = schedule[5] == '1',
+                Saturday = schedule[6] == '1'
+            };
         }
     }
 }
