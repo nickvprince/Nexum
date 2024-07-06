8dc2a090fbd0b421272a7ba4e310f10f95f4334d
Islam Ahmed <islamdoesart@gmail.com>
2024-07-05 23:39:16 -0400

    NEX-316 AES Service Created

---
+++ MSP App/SharedComponents/Utilities/SecurityUtilities.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedComponents.Utilities
+{
+    public class SecurityUtilities
+    {
+        public static string? Encrypt(string? key, string? input)
+        {
+            if(string.IsNullOrEmpty(key) || string.IsNullOrEmpty(input))
+            {
+                return null;
+            }
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = keyBytes;
+                aes.Mode = CipherMode.ECB;
+                aes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter(cs))
+                        {
+                            sw.Write(input);
+                        }
+                    }
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
+        }
+
+        public static string? Decrypt(string? key, string input)
+        {
+            if (string.IsNullOrEmpty(key) || string.IsNullOrEmpty(input))
+            {
+                return null;
+            }
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = keyBytes;
+                aes.Mode = CipherMode.ECB; // Using ECB mode as it does not require an IV
+                aes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream ms = new MemoryStream(Convert.FromBase64String(input)))
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader sr = new StreamReader(cs))
+                        {
+                            return sr.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
