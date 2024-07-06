4b33a4571c520552437ce5a2e67fb5cbcf2b82b6
Islam Ahmed <islamdoesart@gmail.com>
2024-07-05 23:48:35 -0400

    NEX-298 NEX-299 NEX-300 Web API http service added

---
+++ MSP App/App/Services/BackupService.cs
@@ -0,0 +1,124 @@
+﻿using Newtonsoft.Json;
+using SharedComponents.Entities;
+using SharedComponents.Services;
+using SharedComponents.WebRequestEntities.BackupRequests;
+using System.Text;
+
+namespace App.Services
+{
+    public class BackupService : BaseService, IBackupService
+    {
+        public BackupService(IConfiguration config, HttpClient httpClient) : base(config, httpClient)
+        {
+            if (_httpClient.BaseAddress != null)
+            {
+                _httpClient.BaseAddress = new Uri(_httpClient.BaseAddress, "Backup/");
+            }
+            else
+            {
+                throw new InvalidOperationException("BaseAddress is not set.");
+            }
+        }
+
+        public async Task<DeviceBackup?> CreateAsync(BackupCreateRequest request)
+        {
+            try
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                var response = await _httpClient.PostAsync("", content);
+                var responseData = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<DeviceBackup>(responseData);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public async Task<DeviceBackup?> UpdateAsync(BackupUpdateRequest request)
+        {
+            try
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                var response = await _httpClient.PutAsync("", content);
+                var responseData = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<DeviceBackup>(responseData);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            try
+            {
+                var response = await _httpClient.DeleteAsync($"{id}");
+                var responseData = await response.Content.ReadAsStringAsync();
+                response.EnsureSuccessStatusCode();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<DeviceBackup?> GetAsync(int id)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync($"{id}");
+                var responseData = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<DeviceBackup>(responseData);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public async Task<ICollection<DeviceBackup>?> GetAllAsync()
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync("");
+                var responseData = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<ICollection<DeviceBackup>>(responseData);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public async Task<ICollection<DeviceBackup>?> GetAllByDeviceIdAsync(int deviceId)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync($"By-Device/{deviceId}");
+                var responseData = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<ICollection<DeviceBackup>>(responseData);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public async Task<ICollection<DeviceBackup>?> GetAllByTenantIdAsync(int tenantId)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync($"By-Tenant/{tenantId}");
+                var responseData = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<ICollection<DeviceBackup>>(responseData);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ MSP App/SharedComponents/Services/IBackupService.cs
@@ -0,0 +1,21 @@
+﻿using SharedComponents.Entities;
+using SharedComponents.WebRequestEntities.BackupRequests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedComponents.Services
+{
+    public interface IBackupService
+    {
+        public Task<DeviceBackup?> CreateAsync(BackupCreateRequest request);
+        public Task<DeviceBackup?> UpdateAsync(BackupUpdateRequest request);
+        public Task<bool> DeleteAsync(int id);
+        public Task<DeviceBackup?> GetAsync(int id);
+        public Task<ICollection<DeviceBackup>?> GetAllAsync();
+        public Task<ICollection<DeviceBackup>?> GetAllByDeviceIdAsync(int deviceId);
+        public Task<ICollection<DeviceBackup>?> GetAllByTenantIdAsync(int tenantId);
+    }
+}
