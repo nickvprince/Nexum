41e2481cc332e05f7462f2b08fd5e6da36bdff98
Islam Ahmed <islamdoesart@gmail.com>
2024-07-08 15:37:59 -0400

    NEX-319 Service for force_checkin and force_update added, Beat throws notimplemented

---
+++ MSP App/API/Services/HTTPDeviceService.cs
@@ -0,0 +1,69 @@
+﻿using API.DataAccess;
+using API.Services.Interfaces;
+using Newtonsoft.Json;
+using SharedComponents.Utilities;
+using System.Text;
+
+namespace API.Services
+{
+    public class HTTPDeviceService : BaseHTTPService, IHTTPDeviceService
+    {
+        public HTTPDeviceService(IConfiguration config, HttpClient httpClient, AppDbContext appDbContext) : base(config, httpClient, appDbContext)
+        {
+            //Use if tenant server api is updated with proper routes
+            /*if (_httpClient.BaseAddress != null)
+            {
+                _httpClient.BaseAddress = new Uri(_httpClient.BaseAddress, "Device/");
+            }
+            else
+            {
+                throw new InvalidOperationException("BaseAddress is not set.");
+            }*/
+        }
+
+        public async Task<bool> ForceDeviceCheckinAsync(int tenantId, int client_id)
+        {
+            try
+            {
+                if (await InitiallizeHttpClient(tenantId))
+                {
+                    var content = new StringContent(JsonConvert.SerializeObject(client_id, new InvalidJsonUtilities()), Encoding.UTF8, "application/json");
+                    var response = await _httpClient.PostAsync("force_checkin", content);
+                    var responseData = await response.Content.ReadAsStringAsync();
+                    response.EnsureSuccessStatusCode();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error forcing device checkin on the server.");
+            }
+            return false;
+        }
+
+        public async Task<bool> ForceDeviceUpdateAsync(int tenantId, int client_id)
+        {
+            try
+            {
+                if (await InitiallizeHttpClient(tenantId))
+                {
+                    var content = new StringContent(JsonConvert.SerializeObject(client_id, new InvalidJsonUtilities()), Encoding.UTF8, "application/json");
+                    var response = await _httpClient.PostAsync("force_update", content);
+                    var responseData = await response.Content.ReadAsStringAsync();
+                    response.EnsureSuccessStatusCode();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error forcing device update on the server.");
+            }
+            return false;
+        }
+
+        public Task<bool> GetDeviceBeatAsync(int tenantId, int client_id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MSP App/API/Services/Interfaces/IHTTPDeviceService.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Services.Interfaces
+{
+    public interface IHTTPDeviceService
+    {
+        public Task<bool> ForceDeviceCheckinAsync(int tenantId, int client_id);
+        public Task<bool> ForceDeviceUpdateAsync(int tenantId, int client_id);
+        public Task<bool> GetDeviceBeatAsync(int tenantId, int client_id);
+    }
+}
