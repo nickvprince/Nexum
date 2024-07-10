e346b664590e4398f4372665ced22d7b359dcf03
Islam Ahmed <islamdoesart@gmail.com>
2024-07-10 14:12:27 -0400

    NEX-329 Fixed mismatched includes for [JsonIgnore]

--- MSP App/SharedComponents/Entities/ApplicationUser.cs
+++ MSP App/SharedComponents/Entities/ApplicationUser.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace SharedComponents.Entities
 {
