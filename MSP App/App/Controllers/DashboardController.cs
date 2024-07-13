177415f201588045c95828faafebaebf87757805
Islam Ahmed <islamdoesart@gmail.com>
2024-07-13 12:23:58 -0400

    NEX-362 NEX-333 Dashboard Controller started

---
+++ MSP App/App/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using App.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly TenantService _tenantService;
+
+        public DashboardController(TenantService tenantService)
+        {
+            _tenantService = tenantService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
