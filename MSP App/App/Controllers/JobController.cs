7fadb00b46692b9cba2a896851e4a480b1006d2d
Islam Ahmed <islamdoesart@gmail.com>
2024-07-21 19:08:48 -0400

    NEX-343 Dashboard layout complete, repetitive view code put in partials

--- MSP App/App/Controllers/JobController.cs
+++ MSP App/App/Controllers/JobController.cs
@@ -1,23 +1,18 @@
-﻿using App.Services;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using SharedComponents.Entities;
-using SharedComponents.Entities.DbEntities;
-using SharedComponents.Services.APIRequestServices.Interfaces;
 
 namespace App.Controllers
 {
     [Authorize]
-    public class AlertController : Controller
+    public class JobController : Controller
     {
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly IAPIRequestAlertService _alertService;
 
-        public AlertController(IAPIRequestAlertService alertService, IHttpContextAccessor httpContextAccessor)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public JobController(IHttpContextAccessor httpContextAccessor)
         {
-            _alertService = alertService;
             _httpContextAccessor = httpContextAccessor;
         }
 
@@ -34,13 +29,6 @@ namespace App.Controllers
         public async Task<IActionResult> IndexAsync()
         {
             return await Task.FromResult(View());
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Table()
-        {
-            ICollection<DeviceAlert>? alerts = await _alertService.GetAllAsync();
-            return PartialView("_AlertTablePartial", alerts);
         }
     }
 }
