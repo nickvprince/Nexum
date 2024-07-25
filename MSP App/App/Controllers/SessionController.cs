85cb13b5b090041dd0099d805684f1b9102f65a1
Islam Ahmed <islamdoesart@gmail.com>
2024-07-25 14:37:13 -0400

    NEX-343 active button higlight added to view/session

---
+++ MSP App/App/Controllers/SessionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedComponents.Entities.WebAppEntities.Requests.Session_Requests;
+
+namespace App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionController : ControllerBase
+    {
+        public SessionController() { }
+
+        [HttpPost("")]
+        public async Task<IActionResult> SetActiveNavLink([FromBody] SetActiveNavLinkRequest request)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!string.IsNullOrEmpty(request.ActiveLinkId))
+                {
+                    HttpContext.Session.SetString("ActiveNavLink", request.ActiveLinkId);
+                    return await Task.FromResult(Ok());
+                }
+            }
+            return await Task.FromResult(BadRequest("Invalid active link ID."));
+        }
+    }
+}
