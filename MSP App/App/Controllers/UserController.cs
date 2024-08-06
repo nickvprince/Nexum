56502c97969a77b14ce94a97a9d4ed0ecce2f88b
Islam Ahmed <islamdoesart@gmail.com>
2024-08-06 02:22:02 -0400

    NEX-350 simple user table added

--- MSP App/App/Controllers/UserController.cs
+++ MSP App/App/Controllers/UserController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using SharedComponents.Entities.DbEntities;
+using SharedComponents.Entities.WebEntities.Responses.UserResponses;
+using SharedComponents.Services.APIRequestServices.Interfaces;
 
 namespace App.Controllers
 {
     [Authorize]
+    [Route("[controller]")]
     public class UserController : Controller
     {
+        private readonly IAPIRequestUserService _userService;
+
+        public UserController(IAPIRequestUserService userService)
+        {
+            _userService = userService;
+        }
+
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             // Dynamically assign the return URL
@@ -21,6 +32,28 @@ namespace App.Controllers
         public async Task<IActionResult> IndexAsync()
         {
             return await Task.FromResult(View());
+        }
+
+        [HttpGet("Table")]
+        public async Task<IActionResult> TableAsync()
+        {
+            ICollection<UserResponse>? userResponses = await _userService.GetAllAsync();
+            ICollection<ApplicationUser>? users = new List<ApplicationUser>();
+            if (userResponses != null)
+            {
+                foreach (var userResponse in userResponses)
+                {
+                    users.Add(new ApplicationUser
+                    {
+                        Id = userResponse.Id,
+                        UserName = userResponse.UserName,
+                        FirstName = userResponse.FirstName,
+                        LastName = userResponse.LastName,
+                        Type = userResponse.Type
+                    });
+                }
+            }
+            return await Task.FromResult(PartialView("_UserTablePartial", users));
         }
     }
 }
